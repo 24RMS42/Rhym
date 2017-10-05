3c15501c2006a428f1684637dbec29b0176c9fed
24RMS42 <omura.dai113@outlook.com>
2017-10-05 20:20:23 +0200

    Request permission

--- Droid/BrowseMusicActivity.cs
+++ Droid/BrowseMusicActivity.cs
@@ -3,12 +3,16 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
+using Android;
 using Android.App;
 using Android.Content;
+using Android.Content.PM;
 using Android.Database;
 using Android.OS;
 using Android.Provider;
 using Android.Runtime;
+using Android.Support.Design.Widget;
 using Android.Views;
 using Android.Widget;
 
@@ -17,16 +21,41 @@ namespace Rhym.Droid
     [Activity(Label = "Select Music")]
     public class BrowseMusicActivity : Activity
     {
+        readonly string[] Permissions =
+        {
+            Manifest.Permission.ReadExternalStorage
+        };
+
+        const int RequestLocationId = 0;
+        List<SongModel> songsList;
+        ListView listView;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
             SetContentView(Resource.Layout.BrowseMusic);
-            ListView listView = FindViewById<ListView>(Resource.Id.listview);
+            listView = FindViewById<ListView>(Resource.Id.listview);
             Button btnImport = FindViewById<Button>(Resource.Id.btn_add);
 
-            List<SongModel> songsList;
+            TryGetLocalMusic();
 
+            btnImport.Click += (sender, e) => 
+            {
+                OnBackPressed();
+                App.G_SongList = new ObservableCollection<SongModel>();
+                foreach (var item in songsList)
+                {
+                    if (item.IsSelected)
+                    {
+                        App.G_SongList.Add(item);
+                    }
+                }
+            };
+        }
+
+        void GetLocalMusic()
+        {
             var uri = MediaStore.Audio.Media.ExternalContentUri;
             string[] projection = {
                 MediaStore.Audio.Media.InterfaceConsts.Id,
@@ -52,19 +81,67 @@ namespace Rhym.Droid
 
             listView.Adapter = new SongAdapter(this, cursor, songsList);
             Console.WriteLine("songlist:" + songsList.Count);
+        }
 
-            btnImport.Click += (sender, e) => 
-            {
-                OnBackPressed();
-                App.G_SongList = new ObservableCollection<SongModel>();
-                foreach (var item in songsList)
-                {
-                    if (item.IsSelected)
-                    {
-                        App.G_SongList.Add(item);
-                    }
-                }
-            };
+        void TryGetLocalMusic()
+        {
+        	if ((int)Build.VERSION.SdkInt < 23)
+        	{
+                GetLocalMusic();
+        		return;
+        	}
+
+        	GetPermissionAsync();
+        }
+
+        void GetPermissionAsync()
+        {
+        	//Check to see if any permission in our group is available, if one, then all are
+        	const string permission = Manifest.Permission.ReadExternalStorage;
+        	if (CheckSelfPermission(permission) == (int)Permission.Granted)
+        	{
+              	GetLocalMusic();
+        		return;
+        	}
+
+        	//need to request permission
+        	//if (ShouldShowRequestPermissionRationale(permission))
+        	//{
+        	//	//Explain to the user why we need to read the contacts
+        	//	Snackbar.Make(layout, "Location access is required to show coffee shops nearby.", Snackbar.LengthIndefinite)
+        	//			.SetAction("OK", v => RequestPermissions(PermissionsLocation, RequestLocationId))
+        	//			.Show();
+        	//	return;
+        	//}
+
+        	//Finally request permissions with the list of permissions and Id
+        	RequestPermissions(Permissions, RequestLocationId);
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Permission[] grantResults)
+        {
+        	switch (requestCode)
+        	{
+        		case RequestLocationId:
+        			{
+        				if (grantResults[0] == Permission.Granted)
+        				{
+        					//Permission granted
+        					//var snack = Snackbar.Make(layout, "Location permission is available, getting lat/long.", Snackbar.LengthShort);
+        					//snack.Show();
+
+        					GetLocalMusic();
+        				}
+        				else
+        				{
+        					//Permission Denied 🙁
+        					//Disabling location functionality
+        					//var snack = Snackbar.Make(layout, "Location permission is denied.", Snackbar.LengthShort);
+        					//snack.Show();
+        				}
+        			}
+                    break;
+            }
         }
     }
 }
